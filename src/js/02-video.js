4aade7be44ec1c3ca46554d3d93038e845ce260d
Elena Pedko <elpg.ods@gmail.com>
2022-10-31 21:37:29 +0200

    Task 2 is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(currentTimeUpdate, 1000));
+
+function currentTimeUpdate(evt) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(evt.seconds));
+};
+
+const currentTimeContinue = localStorage.getItem(STORAGE_KEY);
+if (currentTimeContinue) {
+    player.setCurrentTime(currentTimeContinue);
+};
