f53c2d5e6e0e54579226fe998d4fa7620ddb3a3e
Elena Pedko <elpg.ods@gmail.com>
2022-11-01 14:44:40 +0200

    Mistakes in task 3 are corrected

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,6 +11,13 @@ populateInput();
 
 function onFormSubmit(evt) {
 evt.preventDefault();
+
+const formData = {
+    email: evt.target.email.value,
+    message: evt.target.message.value
+};
+console.log(formData);
+
 evt.target.reset();
 localStorage.removeItem(STORAGE_KEY);
 }
@@ -18,15 +25,15 @@ localStorage.removeItem(STORAGE_KEY);
 function onTextInput(evt) {
     const formInfo = { email: email.value , message: textarea.value };
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formInfo));
-    console.log(formInfo);
 }
 
 function populateInput() {
-    const savedInput = localStorage.getItem(STORAGE_KEY);
+    const savedInput = localStorage.getItem(STORAGE_KEY); 
     
     if(savedInput) {
-        console.log(savedInput);
-        onTextInput.value = savedInput;
+        const parsedInput = JSON.parse(savedInput);
+        email.value = parsedInput.email;
+        textarea.value = parsedInput.message;
     }
 }
 
